314d5f91a059cb1169f84e6abb8495a767c104e7
Aniket Chavan <chavananiket1272@gmail.com>
2024-08-08 23:34:18 +0530

    Your commit message here

--- backend/routes/instafeeds.js
+++ backend/routes/instafeeds.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const axios = require("axios");
+const fetch = require("node-fetch");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN; // Store securely in environment variables
 
   try {
-    const response = await axios.get(`https://graph.instagram.com/me/media`, {
-      params: {
-        fields: "id,caption,media_url,media_type,permalink",
-        access_token: accessToken,
-      },
-    });
-    res.json(response.data);
+    const response = await fetch(
+      `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${accessToken}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
     console.error("Error fetching Instagram posts:", error);
     res.status(500).send("Failed to fetch Instagram posts");
