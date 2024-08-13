3e5772b73391b31aade2bc98dc4bb6709e56840c
Aniket Chavan <chavananiket1272@gmail.com>
2024-08-13 09:41:14 +0530

    Your commit message here

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -62,29 +62,17 @@ function Home() {
   return (
     <div className="relative">
       <Navbar /> {/* Always render Navbar */}
-      <div ref={mainRef}> {/* Only animate components inside mainRef */}
-        <Suspense fallback={<Loader />}>
+      <Suspense fallback={<Loader />}>
+        <div ref={mainRef}> {/* Only animate components inside mainRef */}
           <MainLayout />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <LatestNews />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <Players />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <Gallery />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <PlayerOfMonth />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <InstaFeed />
-        </Suspense>
-        <Suspense fallback={<Loader />}>
           <Footer />
-        </Suspense>
-      </div>
+        </div>
+      </Suspense>
     </div>
   );
 }
