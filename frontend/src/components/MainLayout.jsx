befda10bb5d6261394583c4fdc58edaf8334b4d0
Aniket Chavan <chavananiket1272@gmail.com>
2024-08-24 12:25:50 +0530

    Your commit message here

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,9 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Carousel from './Carousel';
 import RecentFixture from './RecentFixture';
+import Loader from '../components/Loader'; // Import the loader
 
 const MainLayout = () => {
+  const [isCarouselLoading, setIsCarouselLoading] = useState(true);
+  const [isRecentFixtureLoading, setIsRecentFixtureLoading] = useState(true);
   const carouselRef = useRef(null);
+
+  useEffect(() => {
+    // Simulate fetching data for both Carousel and RecentFixture
+    setTimeout(() => {
+      setIsCarouselLoading(false);
+      setIsRecentFixtureLoading(false);
+    }, 3000); // 3-second delay to simulate data fetching
+  }, []);
+
+  if (isCarouselLoading || isRecentFixtureLoading) {
+    return (
+      <div className="absolute inset-0 flex items-center justify-center bg-white">
+        <Loader /> {/* Using your custom Loader component */}
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col lg:flex-row lg:justify-between lg:items-stretch max-w-7xl mx-auto mt-10 lg:mt-24 w-full">
