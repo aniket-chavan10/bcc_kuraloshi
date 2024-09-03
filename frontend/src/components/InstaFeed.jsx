3c6639ab5453520afa3974abafafc617f614deec
Aniket Chavan <chavananiket1272@gmail.com>
2024-09-03 11:24:15 +0530

    Your commit message here

--- frontend/src/components/InstaFeed.jsx
+++ frontend/src/components/InstaFeed.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Loader from "../components/Loader";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -55,10 +53,6 @@ const InstaFeed = () => {
     }
   }, [posts]);
 
-  if (loading) {
-    return <Loader text="Fetching Instagram Feeds..." />;
-  }
-
   if (error) {
     return (
       <div className="flex justify-center items-center h-screen text-red-500">
@@ -70,8 +64,13 @@ const InstaFeed = () => {
   return (
     <div className="container mx-auto py-10 mt-3 md:px-0">
       <h2 className="text-2xl font-bold mb-4 px-4">Instagram Posts</h2>
-      <div className="container mx-auto py-10 p-5 md:p-16 bg-custom-gradient">
-        <div className="h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100">
+      <div className="container mx-auto py-10 p-5 md:p-16 bg-custom-gradient relative">
+        {loading && (
+          <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-75 z-10">
+            <Loader />
+          </div>
+        )}
+        <div className={`h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100 ${loading ? 'opacity-50' : ''}`}>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pr-5">
             {posts.map((post) => (
               <a
@@ -82,11 +81,10 @@ const InstaFeed = () => {
                 className="insta-feed-item overflow-hidden block shadow-lg"
               >
                 <div className="bg-white p-1">
-                  <LazyLoadImage
+                  <img
                     src={post.media_url}
                     alt={post.caption}
-                    className="w-full h-auto object-cover transition-transform duration-1000 ease-in-out transform"
-                    effect="blur"
+                    className="w-full h-auto object-cover transition-transform duration-1000 ease-in-out transform "
                   />
                   <div className="p-4">
                     <p className="text-gray-700 font-semibold text-sm truncate leading-tight">
